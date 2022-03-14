e94ef76c46f2d1d3954015fab1be26da0e27923d
LLPROG <94935580+LLPROG@users.noreply.github.com>
2022-03-14 16:17:52 +0000

    add done or not done effect

--- js/script.js
+++ js/script.js
@@ -12,13 +12,15 @@ Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 
 MILESTONE 2
 Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
-MILESTONE 3
 
+MILESTONE 3
 Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 
 
 Bonus:
+
 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+
 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
 Buon lavoro e buon divertimento!
 
@@ -50,11 +52,14 @@ const app = new Vue({
                 text: this.value,
                 done: false,
             }
-            this.arrTodos.unshift(obj);
-            this.value = '';
+            if (this.value != '') {
+                this.arrTodos.unshift(obj);
+                this.value = '';
+            }
+
         },
         deleteTodo(index) {
             this.arrTodos.splice(index, 1)
-        }
+        },
     }
   })
